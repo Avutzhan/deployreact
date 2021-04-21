5d66b9903484f866a35b861ba5d5e0c4e6393bd9
Avutzhan Dautov <dautov92@list.ru>
2021-04-21 21:48:57 +0600

    revert last changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React, { Component } from 'react';
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
