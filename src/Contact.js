0073fadea66e95344bba80597846c783c82154ee
Avutzhan Dautov <dautov92@list.ru>
2021-04-21 21:41:58 +0600

    Add router and pages

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+// Contact.js
+
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact Page</h2>
+                <main>
+                    <p>This section contains information contact...</p>
+                </main>
+            </div>
+        );
+    }
+}
+
+export default Contact;
